1d872f67815bd1846633b5192ad2e64cada104f0
nsathya123 <71782034+nsathya123@users.noreply.github.com>
2021-06-28 13:39:23 +0530

    Update uart.c

--- machine/uart.c
+++ machine/uart.c
@@ -6,28 +6,24 @@
 
 volatile uint32_t* uart;
 
+#define STS_TX_FULL 1 << 1
+
 void uart_putchar(uint8_t ch)
 {
-#ifdef __riscv_atomic
-    int32_t r;
-    do {
-      __asm__ __volatile__ (
-        "amoor.w %0, %2, %1\n"
-        : "=r" (r), "+A" (uart[UART_REG_TXFIFO])
-        : "r" (ch));
-    } while (r < 0);
-#else
-    volatile uint32_t *tx = uart + UART_REG_TXFIFO;
-    while ((int32_t)(*tx) < 0);
-    *tx = ch;
-#endif
+	volatile uint32_t *tx = uart + UART_REG_TXFIFO; 
+
+	while (uart[UART_STATUS] & STS_TX_FULL);
+	*tx = ch;
 }
 
 int uart_getchar()
 {
-  int32_t ch = uart[UART_REG_RXFIFO];
-  if (ch < 0) return -1;
-  return ch;
+	while((uart[UART_STATUS] & STS_RX_NOT_EMPTY) == 0); 
+    
+	uint8_t ch = uart[UART_REG_RXFIFO];
+
+	if (ch <= 0 || ch>127) return -1;
+	return ch;
 }
 
 struct uart_scan
